86f86a7438c46f57402c7832e94f4ff8419a563f
jeshwanthg <jeshwanth.gc.com>
2026-01-07 12:31:02 +0100

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 import dill 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from catboost import CatBoostRegressor
 
 
 from src.exception import CustomException
@@ -22,31 +24,36 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
+        for i in range(len(models)):
+            model_name = list(models.keys())[i]
             model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+            para = param[model_name]
 
-            # gs = GridSearchCV(model,para,cv=3)
-            # gs.fit(X_train,y_train)
+            # 🔴 CATBOOST HANDLING (NO GridSearchCV)
+            if isinstance(model, CatBoostRegressor):
+                model.fit(X_train, y_train)
 
-            # model.set_params(**gs.best_params_)
-            # model.fit(X_train,y_train)
+            # 🟢 SKLEARN MODELS
+            else:
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
 
-            model.fit(X_train, y_train)  # Train model
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
 
+            # Predictions
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
+            # Scores
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
 
         return report
 
